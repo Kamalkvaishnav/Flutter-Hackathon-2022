6d18a20a01fb72170243dd3d9d95defa449e778b
Naval14 <jagginaval1312@gmail.com>
2022-01-22 04:39:18 +0530

    pre booking layout

--- mess_app/lib/preBooking.dart
+++ mess_app/lib/preBooking.dart
@@ -69,37 +69,221 @@ class _PreBookingState extends State<PreBooking> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Color.fromARGB(255, 125, 255, 120),
-        height: 300,
-        width: 200,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text(
-                'Chiken Biryani',
-                style: GoogleFonts.lato(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Color.fromARGB(255, 3, 3, 3),
-                ),
+    return Scaffold(
+      // appBar: AppBar(
+        
+      //   title: const Text('   Mess Portal'),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: const Icon(Icons.add_alert),
+      //       tooltip: 'Show Snackbar',
+      //       onPressed: () {
+      //         ScaffoldMessenger.of(context).showSnackBar(
+      //             const SnackBar(content: Text('This is a snackbar')));
+      //       },
+      //     ),  
+      //   ],
+      // ),
+      
+      body : Stack(
+        children: 
+          [Container(
+            height: MediaQuery.of(context).size.height *0.45, 
+            decoration: BoxDecoration (
+              borderRadius:BorderRadius.circular(40.0),
+              image : DecorationImage(            
+                image: AssetImage("assets/biryani.jpg"),
+                fit : BoxFit.cover,
+                
+                alignment: Alignment.topLeft,
+                
               ),
             ),
-            SizedBox(height: 15),
-            ElevatedButton(
-                onPressed: openCheckOut, child: Text("Pay With RazorPay")),
-            SizedBox(height: 15),
-            ElevatedButton(
-                onPressed: () {
+          ),
+             Column( 
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [ 
+                Container(
+                  height: MediaQuery.of(context).size.height *0.6,
+                  width: MediaQuery.of(context).size.width,
+                  //color: Colors.white,
+                  decoration: BoxDecoration(
+                    color : Colors.white,
+                    shape : BoxShape.rectangle,
+                   borderRadius: new BorderRadius.only(
+                    topLeft: const Radius.circular(50.0),
+                    topRight: const Radius.circular(50.0),
+                  ),
+                    // boxShadow: [
+                    //   BoxShadow( 
+                    //     color : Colors.blue,
+                    //     blurRadius: 6,
+                    //     spreadRadius: 3,
+                    //   ),
+                    // ],
+                  ),
+      
+                  child : Column( 
+                    children: [
+                      SizedBox(height : 25),
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.,
+                        children: 
+                          [Text(
+                            "    Chicken Biryani",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color : Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: 
+                          [Text(
+                            
+                            "Rs. 120   ",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color : Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 30,),
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: 
+                          [Text(
+                            "  Confirm your chicken meal ! ",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color : Colors.black,
+                              //fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    SizedBox(height: 20),
+                     Row(
+                       // mainAxisAlignment: MainAxisAlignment,
+                        children: 
+                          [Text(
+                            "   . Payment Method",
+                            style: TextStyle(
+                              fontSize: 24,
+                              color : Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    SizedBox(height: 40),
+
+                     
+                   
+                      Container(
+                        height : 50,
+                        width :MediaQuery.of(context).size.width*0.7,
+                        //color: Colors.yellow,
+                        child :ElevatedButton(
+                          style :ElevatedButton.styleFrom(
+                            side: BorderSide(
+                              width: 2.0,
+                              color: Colors.blue,
+                            ),
+                            shape: RoundedRectangleBorder( 
+                              borderRadius: BorderRadius.circular(30)
+                            ),
+                            primary: Colors.white
+                          ),
+                         child : Row(
+                           
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+      
+            Text(
+              '',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.blue,
+              ),
+            ),
+            // ImageIcon(
+            //   AssetImage("Images/upi.png"),
+            //   size : 20,
+            // )
+            Image.asset(
+                "assets/upi.png"
+            ),
+          ],
+          ),
+                          
+                          onPressed: () {
                   Navigator.pushNamed(context, '/upi');
                 },
-                child: Text("Pay With UPI"))
+                        ),
+                       
+                      ),
+SizedBox(height: 40),
+                      Container(
+                        height : 50,
+                        width :MediaQuery.of(context).size.width*0.7,
+                        //color: Colors.yellow,
+                        child :ElevatedButton(
+                          style :ElevatedButton.styleFrom(
+                            side: BorderSide(
+                              width: 2.0,
+                              color: Colors.blue,
+                            ),
+                            shape: RoundedRectangleBorder( 
+                              borderRadius: BorderRadius.circular(30)
+                            ),
+                            primary: Colors.white
+                          ),
+                         child : Row(
+                           
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+      
+            Text(
+              '',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.blue,
+              ),
+            ),
+            // ImageIcon(
+            //   AssetImage("Images/upi.png"),
+            //   size : 20,
+            // )
+            Image.asset(
+                "assets/razor.png",
+            ),
           ],
-        ),
+          ),
+                          
+                         onPressed: openCheckOut,
+                        ),
+                       
+                      ),
+      
+                    ],
+                  ),
+                    
+                 ),
+              ],
+             ),  
+        ],
       ),
+      
     );
   }
 }
